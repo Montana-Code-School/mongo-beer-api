bd4a3dd749ae1104901805e48ca434da379a6d53
Nathan Talley <nathantalley@Nathans-MacBook-Pro.local>
2017-10-31 19:08:31 -0600

    got the things

---
+++ src/client/js/components/Beer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios'
+
+export default class Beer extends React.Component {
+  constructor(){
+    super();
+    this.state = {beers : []};
+  }
+
+
+  componentDidMount(){
+    axios.get('localhost:3000/beers')
+    .then(function(response) {
+      this.setState({beers: response})
+    })
+  }
+  render(){
+    return (
+    <div>
+      <h3>
+        Beer
+        <ul>{this.state.beers.map((beer)=> <li>beer.name</li>)}</ul>
+      </h3>
+    </div>
+    )
+  }
+}
---
+++ src/client/js/components/Brewery.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Brewery extends React.Component {
+
+
+  render(){
+    return (
+    <div>
+      <h3>
+        Brewery
+      </h3>
+    </div>
+    )
+  }
+}
