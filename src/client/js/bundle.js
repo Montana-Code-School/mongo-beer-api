0aaa511f92912ce651453b6cf619d5a4ccae8f35
Nathan Talley <nathantalley@Nathans-MacBook-Pro.local>
2017-10-31 18:29:24 -0600

    you you

--- src/client/js/bundle.js
+++ src/client/js/bundle.js
@@ -60,11 +60,12 @@
 /******/ 	__webpack_require__.p = "/";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 42);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
+/******/ ({
+
+/***/ 42:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -73,5 +74,6 @@
 alert('blah blah');
 
 /***/ })
-/******/ ]);
+
+/******/ });
 //# sourceMappingURL=bundle.js.map
